e9e9667fda0f038d71b302d1a677d5a2501f6b48
TYCOELECTRONICS\TE434973 <mohammad.abedin@te.com>
2022-08-09 16:50:43 +0530

    ep003 change

---
+++ Ep002/build/CMakeFiles/HellAppBinary.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HellAppBinary.dir/main.cpp.obj"
+  "CMakeFiles/HellAppBinary.dir/main.cpp.obj.d"
+  "HellAppBinary.exe"
+  "HellAppBinary.exe.manifest"
+  "HellAppBinary.pdb"
+  "libHellAppBinary.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HellAppBinary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
